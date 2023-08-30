65637f9dd563216cf996d1423579ed2f9e84ba49
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-08-31 08:33:20 +1000

    Add #141 Linked List Cycle solution

--- neetcode150.cpp
+++ neetcode150.cpp
@@ -1496,5 +1496,45 @@ public:
         return head;
       };
     };
+
+    class linkedListCycle {
+      struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode(int x) : val(x), next(NULL) {}
+      };
+
+      bool hasCycle(ListNode *head) {
+        if (head == nullptr || head->next == nullptr) {
+          return false;
+        }
+
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+
+        bool hasCycle = false;
+
+        while (true) {
+          slow = slow->next;
+
+          fast = fast->next;
+          if (fast == nullptr) {
+            break;
+          } else if (fast == slow) {
+            hasCycle = true;
+            break;
+          }
+
+          fast = fast->next;
+          if (fast == nullptr) {
+            break;
+          } else if (fast == slow) {
+            hasCycle = true;
+            break;
+          }
+        }
+
+        return hasCycle;
+      };
+    };
   };
-};
