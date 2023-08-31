55b293b376d22c6f1a1bd215f0e997b302610a62
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-08-31 21:39:41 +1000

    Add #104 Maximum Depth of Binary Tree solution

--- neetcode150trees.cpp
+++ neetcode150trees.cpp
@@ -1,27 +1,35 @@
+#include <algorithm>
+
 using namespace std;
 
-class invertBinaryTree {
-  struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right)
-        : val(x), left(left), right(right) {}
-  };
-
-  TreeNode *invertTree(TreeNode *root) {
-    if (root == nullptr) {
-      return;
-    }
-
-    TreeNode *right = invertTree(root->left);
-    TreeNode *left = invertTree(root->right);
-
-    root->left = left;
-    root->right = right;
-
-    return root;
-  }
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
+
+TreeNode *invertTree(TreeNode *root) {
+  if (root == nullptr) {
+    return;
+  }
+
+  TreeNode *right = invertTree(root->left);
+  TreeNode *left = invertTree(root->right);
+
+  root->left = left;
+  root->right = right;
+
+  return root;
+}
+
+int maxDepth(TreeNode *root) {
+  if (root == nullptr) {
+    return 0;
+  }
+
+  return max(maxDepth(root->left) + 1, maxDepth(root->right) + 1);
+}
